84093d4720b309c77fbc950881a50b3572bc1a8b
Christine Marquez <justin.henry124@gmail.com>
2026-03-24 22:31:39 -0400

    final edits

--- app.js
+++ app.js
@@ -2,12 +2,40 @@
 // service_isnormh
 // ZCV5aL5wDiIB1JwGY
 
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX  * scaleFactor ;
+    const y = event.clientY  * scaleFactor ;
+
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+    }
+}
+
+// // Attach mousemove listener to trigger shape movement
+ document.addEventListener("mousemove", moveBackground);
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+    document.body.classList += "dark-theme"}
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
 function contact(event) {
     event.preventDefault();
     const load = document.querySelector('.modal__overlay--load')
 const success = document.querySelector('.modal__overlay--success')
 load.classList += " modal__overlay--visible"
-    
+
 emailjs
      .sendForm(
          'service_isnormh',
@@ -24,9 +52,12 @@ emailjs
     )
 
  })
-setTimeout(() => {
-
-    console.log('it worked')
-}, 1000);
 
 }   
+
+function toggleModal() {
+    document.body.classList.toggle("modal--open");
+}
+
+// Close modal when exit button is clicked
+document.querySelector(".modal__exit").addEventListener("click", toggleModal);
